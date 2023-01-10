397e535c200b7f9b6e63c2b8af72a2092be500eb
Armen Mirzoyan Denisov <100298950+armirzoya@users.noreply.github.com>
2023-01-10 09:39:21 +0100

    Initial commit

--- notification/src/main/java/com/cap/notification/logic/NotificationService.java
+++ notification/src/main/java/com/cap/notification/logic/NotificationService.java
@@ -1,26 +1,45 @@
 package com.cap.notification.logic;
 
 import com.cap.notification.request.ShopOrderRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
 
+    @Autowired
+    private JavaMailSender emailSender;
+
     public void process(ShopOrderRequest request){
         if(request.getSuccess()){
             confirmed(request);
         } else {
-            canceled(request);
+            cancelled(request);
         }
     }
 
     public void confirmed(ShopOrderRequest request) {
 
-        System.out.println("Email to: " + request.getData().getEmail() + " | Order: " + request.getData().getInvoice() + " its confirmed and will be received soon");
+        sendMessage(request.getData().getEmail(), "Confirmation", "Order: " + request.getData().getInvoice() + " its confirmed and will be received soon");
     }
 
-    public void canceled(ShopOrderRequest request) {
+    public void cancelled(ShopOrderRequest request) {
 
-        System.out.println("Email to: " + request.getData().getEmail() + " | Order: " + request.getData().getUuid() + " its canceled");
+        sendMessage(request.getData().getEmail(), "Cancellation", "Order: " + request.getData().getUuid() + " its cancelled");
+    }
+
+    public void sendMessage(String to, String subject, String text) {
+
+        System.out.println("Email to: " + to + " | Status: " + subject + " | " + text);
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+
+        emailSender.send(message);
     }
 }
